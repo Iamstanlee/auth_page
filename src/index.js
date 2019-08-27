9418fdf95c5e06519b30e0c044651c466f00870c
Iamstanlee <stanlee.ak@gmail.com>
2019-08-27 11:15:25 +0100

    removed static files

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 
 const app = (
- <BrowserRouter basename="">
+ <BrowserRouter basename="/authpage/login">
   <App />
  </BrowserRouter>
 );
